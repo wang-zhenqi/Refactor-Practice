b18c75c12eab8d334aab1d529f2d1a74c647eb10
zhenqi <dreamingwzq@gmail.com>
2024-12-12 18:34:27 +0800

    [feat] preparation for the program production plan in chapter 4

---
+++ src/Chapter04-ProductionPlan/producer.py
@@ -0,0 +1,24 @@
+class Producer:
+    def __init__(self, a_province, data):
+        self.province = a_province
+        self.cost = data.cost
+        self.name = data.name
+        self.production = data.production or 0
+
+    def get_name(self):
+        return self.name
+
+    def get_cost(self):
+        return self.cost
+
+    def set_cost(self, arg):
+        self.cost = int(arg)
+
+    def get_production(self):
+        return self.production
+
+    def set_production(self, amount_str):
+        amount = int(amount_str)
+        new_production = amount if amount else 0
+        self.province.set_total_production(self.province.get_total_production() - self.production + new_production)
+        self.production = new_production
---
+++ src/Chapter04-ProductionPlan/province.py
@@ -0,0 +1,62 @@
+from producer import Producer
+
+
+class Province:
+    def __init__(self, doc):
+        self.name = doc.name
+        self.producers = []
+        self.total_production = 0
+        self.demand = doc.demand
+        self.price = doc.price
+        for producer in doc.producers:
+            self.add_producer(Producer(self, producer))
+
+    def add_producer(self, arg):
+        self.producers.append(arg)
+        self.total_production += arg.production
+
+    def get_name(self):
+        return self.name
+
+    def get_producers(self):
+        return self.producers
+
+    def get_total_production(self):
+        return self.total_production
+
+    def set_total_production(self, arg):
+        self.total_production = arg
+
+    def get_demand(self):
+        return self.demand
+
+    def set_demand(self, arg):
+        self.demand = int(arg)
+
+    def get_price(self):
+        return self.price
+
+    def set_price(self, arg):
+        self.price = int(arg)
+
+    def shortfall(self):
+        return self.demand - self.total_production
+
+    def profit(self):
+        return self.demand_value() - self.demand_cost()
+
+    def demand_cost(self):
+        result = 0
+        remaining_demand = self.demand
+
+        for producer in sorted(self.producers, key=lambda x: x.cost):
+            contribution = min(remaining_demand, producer.production)
+            remaining_demand -= contribution
+            result += producer.cost * contribution
+        return result
+
+    def demand_value(self):
+        return self.satisfied_demand() * self.price
+
+    def satisfied_demand(self):
+        return min(self.demand, self.total_production)
