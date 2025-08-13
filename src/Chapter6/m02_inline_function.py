8cc2a396bb1dbb8d6651439f6c08787b6a10d877
zhenqi <dreamingwzq@gmail.com>
2025-08-13 18:25:46 +0800

    [6.2 inline function] put the function inline

--- src/Chapter6/m02_inline_function.py
+++ src/Chapter6/m02_inline_function.py
@@ -1,9 +1,5 @@
-def calculate_total_price(quantity, price):
-    return quantity * price
-
-
 def print_invoice(quantity, price):
-    total = calculate_total_price(quantity, price)
+    total = quantity * price
     print(f"Quantity: {quantity}")
     print(f"Price per item: {price}")
     print(f"Total: {total}")
