c0dfe4db7c72886320bb7f7b3fe27e2fa9c3630b
zhenqi <dreamingwzq@gmail.com>
2025-08-14 16:58:32 +0800

    [6.5 change function declaration] initialize the code and tests for level 2 - remove a parameter

--- src/Chapter6/test_05_change_function_declaration.py
+++ src/Chapter6/test_05_change_function_declaration.py
@@ -1,6 +1,8 @@
-from m05_change_function_declaration import calculate_total_price
-
-from Chapter6.m05_change_function_declaration import apply_discount_and_tax
+from m05_change_function_declaration import (
+    apply_discount_and_tax,
+    calculate_shipping_cost,
+    calculate_total_price,
+)
 
 
 def test_calc_total_price():
@@ -20,3 +22,8 @@ def test_calc_total_price():
 def test_apply_discount():
     assert apply_discount_and_tax(0.2, 88) == 70.4
     assert apply_discount_and_tax(0.3, 90, 0.1) == 69.3
+
+
+def test_calulate_shipping_cost():
+    assert calculate_shipping_cost(10, 100, 0.5, 0.2, True) == 27.5
+    assert calculate_shipping_cost(10, 100, 0.5, 0.2, False) == 25
